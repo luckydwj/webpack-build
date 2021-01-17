cae371616d1a26c01658bcf374172e69a1c4cab2
dwj <dwi@163.com>
2021-01-17 17:53:30 +0800

    dev-server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 /**
  *  Created by daiwenjuan on 2021/1/16 10:41.
  */
-import _ from 'lodash';
-import './style.css'
-import Icon from './icon.jpeg'
-import printMe from './print.js';
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.jpeg";
+import printMe from "./print.js";
 function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  btn.innerHTML = 'Click me and check the console!';
+  btn.innerHTML = "Click me sdfdsf";
   btn.onclick = printMe;
 
   element.appendChild(btn);
--- src/print.js
+++ src/print.js
@@ -2,5 +2,5 @@
  *  Created by daiwenjuan on 2021/1/16 16:25.
  */
 export default function printMe() {
-  console.log('I get called from print.js!');
+  console.log("I get called from print.js!");
 }
