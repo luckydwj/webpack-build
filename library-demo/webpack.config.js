0940298dbbd1df3a20ae582ffd1ac33b0a55f112
daiwenjuan <dwj@hosecloud.com>
2021-01-18 16:13:16 +0800

    环境变量

--- library-demo/webpack.config.js
+++ library-demo/webpack.config.js
@@ -1,20 +1,25 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'demo-numbers.js',
-        library: 'demoNumbers',
-        libraryTarget: 'umd',
-    },
-    externals: {
-        lodash: {
-            commonjs: 'lodash',
-            commonjs2: 'lodash',
-            amd: 'lodash',
-            root: '_',
-        },
-    },
+module.exports =(env)=> {
+    //
+    console.log('NODE_ENV: ', env.NODE_ENV);
+    console.log('Production: ', env.production)
+  return {
+      entry: './src/index.js',
+      output: {
+          path: path.resolve(__dirname, 'dist'),
+          filename: 'demo-numbers.js',
+          library: 'demoNumbers',
+          libraryTarget: 'umd',
+      },
+      externals: {
+          lodash: {
+              commonjs: 'lodash',
+              commonjs2: 'lodash',
+              amd: 'lodash',
+              root: '_',
+          },
+      },
+  }
 };
 
