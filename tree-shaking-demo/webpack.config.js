afc40d15fc6f36bbe45c77718b5cc76a9ca72a97
daiwenjuan <dwj@hosecloud.com>
2021-01-20 18:30:18 +0800

    tree-shaking-demo

--- tree-shaking-demo/webpack.config.js
+++ tree-shaking-demo/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,8 +8,14 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    mode: 'development',
+    mode: 'production',
     optimization: {
         usedExports: true,
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hot Module Replacement',
+        }),
+    ],
 };
